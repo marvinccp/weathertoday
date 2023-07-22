fe4604cdb60a91123b9ba34c83ca7bd0af81c8c0
Marvin Berrio <marvinccp@gmail.com>
2023-07-23 00:04:29 +0200

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <section className="footer-container-section">
       <div className="footer-container">
-        <h5>www.marvinberrio.com</h5>
+        <h5>www.marvinberrio.com 2023</h5>
       </div>
     </section>
   );
